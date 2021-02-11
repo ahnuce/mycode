43206e73f0c68f9b9d08bf9aab77e41e0f545e3f
ahnuce@gmail.com <student@bchd.7139141c-81f1-417a-971c-fe23c2b6d9b0>
2021-02-11 17:12:59 +0000

    Encrypting with vault

--- practice.py
+++ practice.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3\
 
-starbuckers = ["espresso", "mocha", "pumpkin spice", ["white mocha", "egg bites"]]
 
-# print JUST white mocha from this list.
-print(starbuckers[3])
-
-#someone is always late getting their order in. How to return the Last element every time?
-print(starbuckers[-1])
-
-#how to print "egg bites" uwhen theres a list within a list
-
-print(starbuckers[3][0])
-print(type(starbuckers[3]))
-#how to print the first letter of the third element in this list
-print(starbuckers[2][0])
+for x in range(1, 6):
+    
+    
+    
+    print(x)
